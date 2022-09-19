8f3691d6027607e542548cfa165c368d718bf285
coldrain-f <intmain__@naver.com>
2022-09-19 17:16:00 +0900

    feat: 유저 생성 API 구현

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('user')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  /**
+   * 유저 생성 API
+   * @returns 생성된 유저의 아이디 값(PK)
+   */
+  @Post()
+  async signup(): Promise<{ userId: number }> {
+    return await this.usersService.signup();
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+  ) {}
+
+  /**
+   * 유저 생성
+   * @returns 생성된 유저의 아이디 값(PK)
+   */
+  async signup(): Promise<{ userId: number }> {
+    const userEntity: User = new User();
+    const savedUser = await this.usersRepository.save(userEntity);
+    return {
+      userId: savedUser.id,
+    };
+  }
+}
