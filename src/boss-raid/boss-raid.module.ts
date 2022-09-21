e26ce3e1eb3188e58f0b0017b6082d94147e75f1
coldrain-f <intmain__@naver.com>
2022-09-21 13:23:12 +0900

    refactor: staticData를 매번 요청하지 않고, Redis에 캐싱하도록 변경

--- src/boss-raid/boss-raid.module.ts
+++ src/boss-raid/boss-raid.module.ts
@@ -1,9 +1,17 @@
 import { HttpModule } from '@nestjs/axios';
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { BossRaidService } from './boss-raid.service';
+import * as redisStore from 'cache-manager-ioredis';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: '127.0.0.1',
+      port: 6379,
+    }),
+    HttpModule,
+  ],
   providers: [BossRaidService],
   exports: [BossRaidService],
 })
--- src/boss-raid/boss-raid.service.ts
+++ src/boss-raid/boss-raid.service.ts
@@ -1,52 +1,69 @@
 import { HttpService } from '@nestjs/axios';
-import {
-  BadRequestException,
-  HttpException,
-  HttpStatus,
-  Injectable,
-} from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 
 export interface BossRaidInfo {
-  bossRaids: [
-    bossRaidLimitSeconds: number,
-    levels: { level: number; score: number }[],
-  ];
+  bossRaidLimitSeconds: number;
+  levels: { level: number; score: number }[];
 }
 
 @Injectable()
 export class BossRaidService {
-  constructor(private readonly httpService: HttpService) {}
-
-  private async fetchBossRaidCachingData() {
-    // Todo: Redis에 올려놓고 캐싱하여 사용하도록 변경
-    const BOSS_RAID_DATA_URL =
-      'https://dmpilf5svl7rv.cloudfront.net/assignment/backend/bossRaidData.json';
-
-    return this.httpService.axiosRef.get(BOSS_RAID_DATA_URL);
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly cacheManager: Cache,
+  ) {
+    // 싱글톤 객체 생성 시 필요한 Redis 캐시 데이터 초기화
+    this.initBossRaidStaticDataCaching();
   }
 
+  /**
+   * 캐싱한 staticData 반환
+   */
+  private async fetchBossRaidCachingData(): Promise<BossRaidInfo> {
+    return await this.cacheManager.get<BossRaidInfo>('bossRaidInfo');
+  }
+
+  /**
+   * 레벨에 따른 보스 레이드 스코어 반환
+   */
   async getBossRaidScore(level: number) {
     if (!this.existBossRaidLevel) {
       throw new BadRequestException('존재하지 않는 레벨입니다.');
     }
-    const response = await this.fetchBossRaidCachingData();
-    const staticData = response.data.bossRaids[0];
-    return staticData.levels[level].score;
+    const bossRaidInfo: BossRaidInfo = await this.fetchBossRaidCachingData();
+    return bossRaidInfo.levels[level].score;
   }
 
   /**
    * 게임 제한시간 seconds로 반환
    */
   async getBossRaidLimitSeconds() {
-    const response = await this.fetchBossRaidCachingData();
-    const staticData = response.data.bossRaid[0];
-    return staticData.bossRaidLimitSeconds;
+    const bossRaidInfo = await this.fetchBossRaidCachingData();
+    return bossRaidInfo.bossRaidLimitSeconds;
   }
 
+  /**
+   * staticData에 존재하는 레벨 범위인지 체크하여 true/false 반환
+   */
   private existBossRaidLevel(level: number): boolean {
     if (level < 0 || level > 3) {
       return false;
     }
     return true;
+  }
+
+  /**
+   * staticData Redis에 캐싱
+   */
+  private async initBossRaidStaticDataCaching() {
+    const BOSS_RAID_DATA_URL =
+      'https://dmpilf5svl7rv.cloudfront.net/assignment/backend/bossRaidData.json';
+
+    const response = await this.httpService.axiosRef.get(BOSS_RAID_DATA_URL);
+    const bossRaidInfo: BossRaidInfo = response.data.bossRaid[0];
+
+    // Todo: Redis에 staticData 캐싱
+    await this.cacheManager.set('bossRaidInfo', bossRaidInfo, { ttl: 0 });
   }
 }
